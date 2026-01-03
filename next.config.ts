a080f7d676e5954e5fe2570471a83891275d82c5
Kevin Du <dxiaochuan@gmail.com>
2026-01-03 15:20:20 +1100

    added local env

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '54321',
+      },
     ],
   },
 };
